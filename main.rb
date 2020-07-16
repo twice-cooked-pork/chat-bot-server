cd78d1d5ab39864195fa751d7772d1c64f9d2adf
Kazuya Izumi <shikibu9419@gmail.com>
2020-07-16 23:21:21 +0900

    Return recipes as json on GET /

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
-require "sinatra"
-require "./get_from_rakuten"
+require 'sinatra'
+require 'json'
+require './elasticsearch_client'
 
-get "/" do
-  "Hello wolrd"
+get '/' do
+  # 材料について牛乳でOR検索した結果を返す
+  ElasticsearchClient.new.search_by_materials(['牛乳'])['hits']['hits'].to_json
 end
