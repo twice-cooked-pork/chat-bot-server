920f661173f323bcbd7f0f8f31d1dde1930d75d9
Kazuya Izumi <shikibu9419@gmail.com>
2020-07-18 17:59:31 +0900

    Add user input intent pattern

--- linebot_with_watson.rb
+++ linebot_with_watson.rb
@@ -5,21 +5,23 @@ require './watson_client'
 
 Dotenv.load
 
-def line_client
-  @client ||= Line::Bot::Client.new do |config|
-    config.channel_id = ENV.fetch('LINE_CHANNEL_ID')
-    config.channel_secret = ENV.fetch('LINE_CHANNEL_SECRET')
-    config.channel_token = ENV.fetch('LINE_CHANNEL_TOKEN')
-  end
+def add_materials(input)
+  "#{input}を追加するね"
+end
+
+def delete_materials(input)
+  "#{input}を削除するね"
+end
+
+def search_recipes(input)
+  "#{input}で検索するね"
 end
 
 post '/callback' do
   body = request.body.read
 
   signature = request.env['HTTP_X_LINE_SIGNATURE']
-  unless line_client.validate_signature(body, signature)
-    error 400 do 'Bad Request' end
-  end
+  error(400) { 'Bad Request' } unless line_client.validate_signature(body, signature)
 
   events = line_client.parse_events_from(body)
   events.each do |event|
@@ -28,15 +30,20 @@ post '/callback' do
 
     result = WatsonClient.new.send_message(event.message['text'])
 
+    # result[:mode]でどの問合せかを判断
+    # result[:input]が存在する場合はユーザからその後のメッセージがあった場合
     case result[:mode]
     when 'add_materials'
       response = '食材の追加だね。「たまねぎ ピーマン」みたいに入力してね'
+      response = add_materials(result[:input]) if result[:input]
     when 'delete_materials'
       response = 'どの食材が無くなったんだい。「たまねぎ」みたいに食材を入力してね'
-    when 'list_materials'
-      response = 'どの食材が無くなったんだい。「たまねぎ」みたいに食材を入力してね'
+      response = delete_materials(result[:input]) if result[:input]
     when 'search_recipes'
       response = '今日のレシピは回鍋肉にしよう'
+      response = search_materials(result[:input]) if result[:input]
+    when 'list_materials'
+      response = 'どの食材が無くなったんだい。「たまねぎ」みたいに食材を入力してね'
     when 'check_materials'
       response = '今は愛の在庫が切れてるよ。買いに行かなくちゃ。'
     when 'cancel_selection'
@@ -51,4 +58,18 @@ post '/callback' do
   end
 
   'OK'
+end
+
+helpers do
+  def line_client
+    @line_client ||= Line::Bot::Client.new do |config|
+      config.channel_id = ENV.fetch('LINE_CHANNEL_ID')
+      config.channel_secret = ENV.fetch('LINE_CHANNEL_SECRET')
+      config.channel_token = ENV.fetch('LINE_CHANNEL_TOKEN')
+    end
+  end
+
+  def elastic_search_client
+    @elastic_search_client ||= ElasticsearchClient.new 'recipe'
+  end
 end
